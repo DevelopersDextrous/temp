5ca6b1240e3744ad0c700d20c0e4d1cce3ab3f91
unleash-shaown <shaown229@yahoo.com>
2014-11-17 23:32:43 +0600

    new table created

---
+++ app/models/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Category extends Eloquent{
+    protected $fillable = array('name');
+    public static $rules = array('name' => 'required|min:3');
+    
+    public function products() {
+        return $this->hasMany('Product');
+    }
+}
