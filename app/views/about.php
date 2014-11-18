aac1d21fd117c83a6c76af790a52e6928bcdcf2d
unleash-shaown <shaown229@yahoo.com>
2014-11-18 08:49:39 +0600

    two tables created

---
+++ app/views/about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?> 
+        <h1>Hello Laravel 4!!!!</h1>
+        <h2>Welcome to the awesomeness.</h2>
+            
+    </body>
+</html>
